e9f3364121ad6a51f61a70d36cec6cd9f13281e7
Ismael <=>
2024-08-05 01:58:03 +0200

    feat: Reactor model created

---
+++ backend/src/main/java/com/ihl95/nuclear/enums/ReactorType.java
@@ -0,0 +1,8 @@
+package com.ihl95.nuclear.enums;
+
+public enum ReactorType {
+  BOILING_WATER,    // Reactor de agua en ebullición
+  PRESSURIZED_WATER, // Reactor de agua a presión
+  CANDU,           // Reactor CANDU
+  OTHER            // Otro tipo de reactor
+}
