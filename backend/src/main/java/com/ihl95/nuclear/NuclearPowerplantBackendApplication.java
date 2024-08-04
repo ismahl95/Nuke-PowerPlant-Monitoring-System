1cee8ec852d240dea47f3d57560b13b668639e60
Ismael <=>
2024-08-05 01:14:50 +0200

    fix: Main class refactored and gitignored incorporated.

--- backend/src/main/java/com/ihl95/nuclear/NuclearPowerplantBackendApplication.java
+++ backend/src/main/java/com/ihl95/nuclear/NuclearPowerplantBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NuclearBackendApplication {
+public class NuclearPowerplantBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NuclearBackendApplication.class, args);
+		SpringApplication.run(NuclearPowerplantBackendApplication.class, args);
 		System.out.println("Hello World!");
 	}
 
