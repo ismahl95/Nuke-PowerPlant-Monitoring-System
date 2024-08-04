ccdc664d3be57c99871f732b67c26081815db4e4
Ismael <=>
2024-08-04 21:54:48 +0200

    chore: Initial commit

---
+++ backend/src/main/java/com/ihl95/nuclear/NukePowerplantBackendApplication.java
@@ -0,0 +1,14 @@
+package com.ihl95.nuclear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuclearBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuclearBackendApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ backend/src/test/java/com/ihl95/nuclear/NuclearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihl95.nuclear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuclearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
