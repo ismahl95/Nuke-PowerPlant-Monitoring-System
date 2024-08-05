e00a75d57f46c7e1f44cc8bf6294db7849d60691
Ismael <=>
2024-08-05 13:35:05 +0200

    feat(Material): Relation with Supplier created.

--- backend/src/main/java/com/ihl95/nuclear/model/Material.java
+++ backend/src/main/java/com/ihl95/nuclear/model/Material.java
@@ -6,6 +6,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.ihl95.nuclear.enums.MaterialType;
 import com.ihl95.nuclear.enums.UnitOfMeasure;
@@ -20,9 +22,16 @@ public class Material {
     private Long id;
 
     private String name; // Nombre
+
     @Enumerated(EnumType.STRING)
     private MaterialType type; // Tipo (combustible, refrigerante, etc.)
+
     private int quantity; // Cantidad
+
     @Enumerated(EnumType.STRING)
     private UnitOfMeasure unitOfMeasure; // Unidad de medida (litros, kg, etc.)
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id") // La columna que se usará para la FK
+    private Supplier supplier; // Relación con Supplier
 }
