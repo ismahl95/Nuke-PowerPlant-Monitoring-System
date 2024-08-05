492aff32008b0dede9b7d20ecfc33e747ec7c0f1
Ismael <=>
2024-08-05 09:11:28 +0200

    feat: Sensor model created

--- backend/src/main/java/com/ihl95/nuclear/model/Sensor.java
+++ backend/src/main/java/com/ihl95/nuclear/model/Sensor.java
@@ -1,5 +1,25 @@
 package com.ihl95.nuclear.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class Sensor {
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type; // Tipo de sensor
+    private String location; // Ubicación del sensor
+
+    @OneToMany(mappedBy = "sensor")
+    private List<SensorReading> sensorReadings; // Relación con SensorReading
 }
