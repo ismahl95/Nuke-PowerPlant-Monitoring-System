64a8acf38b6c5dfb2ec50dfd510ba75925797230
Ismael <=>
2024-08-05 09:12:50 +0200

    fix(Training): Relation with operator implemented.

--- backend/src/main/java/com/ihl95/nuclear/model/Training.java
+++ backend/src/main/java/com/ihl95/nuclear/model/Training.java
@@ -8,6 +8,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.ihl95.nuclear.enums.TrainingType;
 
@@ -16,13 +18,17 @@ import lombok.Data;
 @Entity
 @Data
 public class Training {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private String title; // Título
-  private String description; // Descripción
-  @Enumerated(EnumType.STRING)
-  private TrainingType type; // Tipo de capacitación (seguridad, operacional, etc.)
-  private LocalDateTime trainingDate; // Fecha de capacitación
+    private String title; // Título
+    private String description; // Descripción
+    @Enumerated(EnumType.STRING)
+    private TrainingType type; // Tipo de capacitación (seguridad, operacional, etc.)
+    private LocalDateTime trainingDate; // Fecha de capacitación
+
+    @ManyToOne
+    @JoinColumn(name = "operator_id")
+    private Operator operator; // Relación con Operator
 }
