0afcd874a9f346f6f57af00b414fabdda1faa872
Ismael <ismael.h.lamela@gmail.com>
2024-08-21 18:20:10 +0200

    feat: Get all nuclear plants

--- backend/src/main/java/com/ihl95/nuclear/nuclearPlant/application/controller/NuclearPlantController.java
+++ backend/src/main/java/com/ihl95/nuclear/nuclearPlant/application/controller/NuclearPlantController.java
@@ -1,5 +1,7 @@
 package com.ihl95.nuclear.nuclearPlant.application.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ihl95.nuclear.nuclearPlant.application.dto.NuclearPlantCompleteDTO;
+import com.ihl95.nuclear.nuclearPlant.application.dto.NuclearPlantDTO;
 import com.ihl95.nuclear.nuclearPlant.application.service.NuclearPlantService;
 
 @RestController
@@ -17,17 +20,21 @@ public class NuclearPlantController {
     @Autowired
     private NuclearPlantService nuclearPlantService;
 
-/*     @GetMapping
+    @GetMapping
     public ResponseEntity<List<NuclearPlantDTO>> getAllNuclearPlants() {
         List<NuclearPlantDTO> nuclearPlants = nuclearPlantService.getAllNuclearPlants();
         return ResponseEntity.ok(nuclearPlants);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<NuclearPlantDTO> getNuclearPlantById(@PathVariable Long id) {
-        NuclearPlantDTO nuclearPlant = nuclearPlantService.getNuclearPlantById(id);
-        return ResponseEntity.ok(nuclearPlant);
-    } */
+    /*
+     * 
+     * @GetMapping("/{id}")
+     * public ResponseEntity<NuclearPlantDTO> getNuclearPlantById(@PathVariable Long
+     * id) {
+     * NuclearPlantDTO nuclearPlant = nuclearPlantService.getNuclearPlantById(id);
+     * return ResponseEntity.ok(nuclearPlant);
+     * }
+     */
 
     @GetMapping("/complete/{id}")
     public ResponseEntity<NuclearPlantCompleteDTO> getNuclearPlantCompleteById(@PathVariable Long id) {
@@ -35,23 +42,30 @@ public class NuclearPlantController {
         return ResponseEntity.ok(nuclearPlantComplete);
     }
 
-/*     @PostMapping
-    public ResponseEntity<NuclearPlantDTO> createNuclearPlant(@RequestBody NuclearPlantDTO nuclearPlantDTO) {
-        NuclearPlantDTO createdNuclearPlant = nuclearPlantService.createNuclearPlant(nuclearPlantDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdNuclearPlant);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<NuclearPlantDTO> updateNuclearPlant(
-            @PathVariable Long id,
-            @RequestBody NuclearPlantDTO nuclearPlantDTO) {
-        NuclearPlantDTO updatedNuclearPlant = nuclearPlantService.updateNuclearPlant(id, nuclearPlantDTO);
-        return ResponseEntity.ok(updatedNuclearPlant);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteNuclearPlant(@PathVariable Long id) {
-        nuclearPlantService.deleteNuclearPlant(id);
-        return ResponseEntity.noContent().build();
-    } */
+    /*
+     * @PostMapping
+     * public ResponseEntity<NuclearPlantDTO> createNuclearPlant(@RequestBody
+     * NuclearPlantDTO nuclearPlantDTO) {
+     * NuclearPlantDTO createdNuclearPlant =
+     * nuclearPlantService.createNuclearPlant(nuclearPlantDTO);
+     * return ResponseEntity.status(HttpStatus.CREATED).body(createdNuclearPlant);
+     * }
+     * 
+     * @PutMapping("/{id}")
+     * public ResponseEntity<NuclearPlantDTO> updateNuclearPlant(
+     * 
+     * @PathVariable Long id,
+     * 
+     * @RequestBody NuclearPlantDTO nuclearPlantDTO) {
+     * NuclearPlantDTO updatedNuclearPlant =
+     * nuclearPlantService.updateNuclearPlant(id, nuclearPlantDTO);
+     * return ResponseEntity.ok(updatedNuclearPlant);
+     * }
+     * 
+     * @DeleteMapping("/{id}")
+     * public ResponseEntity<Void> deleteNuclearPlant(@PathVariable Long id) {
+     * nuclearPlantService.deleteNuclearPlant(id);
+     * return ResponseEntity.noContent().build();
+     * }
+     */
 }
--- backend/src/main/java/com/ihl95/nuclear/nuclearPlant/application/service/NuclearPlantService.java
+++ backend/src/main/java/com/ihl95/nuclear/nuclearPlant/application/service/NuclearPlantService.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ihl95.nuclear.nuclearPlant.application.dto.NuclearPlantCompleteDTO;
+import com.ihl95.nuclear.nuclearPlant.application.dto.NuclearPlantDTO;
 import com.ihl95.nuclear.nuclearPlant.application.exception.NuclearPlantException;
 import com.ihl95.nuclear.nuclearPlant.application.mapper.NuclearPlantCompleteMapper;
 import com.ihl95.nuclear.nuclearPlant.domain.NuclearPlant;
 import com.ihl95.nuclear.nuclearPlant.infraestructure.NuclearPlantRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
@@ -18,24 +22,25 @@ public class NuclearPlantService {
     @Autowired
     private NuclearPlantRepository nuclearPlantRepository;
 
-    /* @Autowired
-    private NuclearPlantMapper nuclearPlantMapper; */
-
     @Autowired
     private NuclearPlantCompleteMapper nuclearPlantCompleteMapper;
 
-/*     public List<NuclearPlantDTO> getAllNuclearPlants() {
+    public List<NuclearPlantDTO> getAllNuclearPlants() {
         List<NuclearPlant> nuclearPlants = nuclearPlantRepository.findAll();
         return nuclearPlants.stream()
-                .map(nuclearPlantMapper::toNuclearPlantDTO)
+                .map(nuclearPlantCompleteMapper::toNuclearPlantDTO)
                 .collect(Collectors.toList());
     }
 
-    public NuclearPlantDTO getNuclearPlantById(Long id) {
-        NuclearPlant nuclearPlant = nuclearPlantRepository.findById(id)
-                .orElseThrow(() -> NuclearPlantException.notFound("Nuclear Plant not found with id: " + id));
-        return nuclearPlantMapper.toNuclearPlantDTO(nuclearPlant);
-    } */
+    /*
+     * 
+     * public NuclearPlantDTO getNuclearPlantById(Long id) {
+     * NuclearPlant nuclearPlant = nuclearPlantRepository.findById(id)
+     * .orElseThrow(() ->
+     * NuclearPlantException.notFound("Nuclear Plant not found with id: " + id));
+     * return nuclearPlantMapper.toNuclearPlantDTO(nuclearPlant);
+     * }
+     */
 
     public NuclearPlantCompleteDTO getNuclearPlantCompleteById(Long id) {
         NuclearPlant nuclearPlant = nuclearPlantRepository.findById(id)
@@ -43,26 +48,33 @@ public class NuclearPlantService {
         return nuclearPlantCompleteMapper.toNuclearPlantCompleteDTO(nuclearPlant);
     }
 
-/*     public NuclearPlantDTO createNuclearPlant(NuclearPlantDTO nuclearPlantDTO) {
-        NuclearPlant nuclearPlant = nuclearPlantMapper.toNuclearPlant(nuclearPlantDTO);
-        NuclearPlant savedNuclearPlant = nuclearPlantRepository.save(nuclearPlant);
-        return nuclearPlantMapper.toNuclearPlantDTO(savedNuclearPlant);
-    }
-
-    public NuclearPlantDTO updateNuclearPlant(Long id, NuclearPlantDTO nuclearPlantDTO) {
-        if (!nuclearPlantRepository.existsById(id)) {
-            throw NuclearPlantException.notFound("Nuclear Plant not found with id: " + id);
-        }
-        NuclearPlant nuclearPlant = nuclearPlantMapper.toNuclearPlant(nuclearPlantDTO);
-        nuclearPlant.setId(id);
-        NuclearPlant updatedNuclearPlant = nuclearPlantRepository.save(nuclearPlant);
-        return nuclearPlantMapper.toNuclearPlantDTO(updatedNuclearPlant);
-    }
-
-    public void deleteNuclearPlant(Long id) {
-        if (!nuclearPlantRepository.existsById(id)) {
-            throw NuclearPlantException.notFound("Nuclear Plant not found with id: " + id);
-        }
-        nuclearPlantRepository.deleteById(id);
-    } */
+    /*
+     * public NuclearPlantDTO createNuclearPlant(NuclearPlantDTO nuclearPlantDTO) {
+     * NuclearPlant nuclearPlant =
+     * nuclearPlantMapper.toNuclearPlant(nuclearPlantDTO);
+     * NuclearPlant savedNuclearPlant = nuclearPlantRepository.save(nuclearPlant);
+     * return nuclearPlantMapper.toNuclearPlantDTO(savedNuclearPlant);
+     * }
+     * 
+     * public NuclearPlantDTO updateNuclearPlant(Long id, NuclearPlantDTO
+     * nuclearPlantDTO) {
+     * if (!nuclearPlantRepository.existsById(id)) {
+     * throw NuclearPlantException.notFound("Nuclear Plant not found with id: " +
+     * id);
+     * }
+     * NuclearPlant nuclearPlant =
+     * nuclearPlantMapper.toNuclearPlant(nuclearPlantDTO);
+     * nuclearPlant.setId(id);
+     * NuclearPlant updatedNuclearPlant = nuclearPlantRepository.save(nuclearPlant);
+     * return nuclearPlantMapper.toNuclearPlantDTO(updatedNuclearPlant);
+     * }
+     * 
+     * public void deleteNuclearPlant(Long id) {
+     * if (!nuclearPlantRepository.existsById(id)) {
+     * throw NuclearPlantException.notFound("Nuclear Plant not found with id: " +
+     * id);
+     * }
+     * nuclearPlantRepository.deleteById(id);
+     * }
+     */
 }
