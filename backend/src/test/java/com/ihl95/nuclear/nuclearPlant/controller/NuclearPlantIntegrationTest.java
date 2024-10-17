b9c2b9b7a6d97674b42cef247baaf90f5ef020b6
Ismael <ismael.h.lamela@gmail.com>
2024-10-17 22:13:34 +0200

    fix(Sonar): New tests to cover the lines requested

--- backend/src/test/java/com/ihl95/nuclear/nuclearPlant/controller/NuclearPlantIntegrationTest.java
+++ backend/src/test/java/com/ihl95/nuclear/nuclearPlant/controller/NuclearPlantIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class NuclearPlantIntegrationTest {
 
     @Autowired
