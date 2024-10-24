322a659aa43ff244262a97e8b20f7055794d5ed5
Ismael <ismael.h.lamela@gmail.com>
2024-10-24 19:29:34 +0200

    fix(Sonar): Syso changed by logger

--- backend/src/main/java/com/ihl95/nuclear/NuclearPowerplantBackendApplication.java
+++ backend/src/main/java/com/ihl95/nuclear/NuclearPowerplantBackendApplication.java
@@ -2,13 +2,16 @@ package com.ihl95.nuclear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class NuclearPowerplantBackendApplication {
 
+	private static final Logger logger = Logger.getLogger(NuclearPowerplantBackendApplication.class.getName());
+
 	public static void main(String[] args) {
 		SpringApplication.run(NuclearPowerplantBackendApplication.class, args);
-		System.out.println("Hello World!");
+		logger.info("La aplicación se ha levantado en el puerto: 8080");
 	}
 
 }
