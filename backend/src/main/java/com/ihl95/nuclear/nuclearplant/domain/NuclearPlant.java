ba817daf33f18d8bfba500ebfd61e7214e1afb51
Ισμαήλ <46920322+ismahl95@users.noreply.github.com>
2025-02-16 09:24:14 +0100

    docs: Nuclear plant class documentation (#70)

--- backend/src/main/java/com/ihl95/nuclear/nuclearplant/domain/NuclearPlant.java
+++ backend/src/main/java/com/ihl95/nuclear/nuclearplant/domain/NuclearPlant.java
@@ -20,30 +20,69 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Representa una planta nuclear.
+ * 
+ * <p>Esta clase es una entidad JPA que se utiliza para mapear la tabla de plantas nucleares en la base de datos.
+ * Incluye información sobre el nombre, ubicación, fechas de creación y modificación, y los usuarios que realizaron
+ * estas acciones.</p>
+ * 
+ * <p>La clase utiliza anotaciones de Lombok para generar automáticamente los constructores, getters, setters y otros
+ * métodos comunes.</p>
+ * 
+ * <p>La clase también está anotada con {@link EntityListeners} para incluir un listener de auditoría que maneja
+ * automáticamente las fechas y usuarios de creación y modificación.</p>
+ * 
+ * @see AuditEntityListener
+ * 
+ * @author Ismahl95
+ */
 @Entity
 @EntityListeners(AuditEntityListener.class)
 @Data
-@NoArgsConstructor // Genera un constructor sin argumentos
-@AllArgsConstructor // Genera un constructor con todos los argumentos
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class NuclearPlant {
 
+    /**
+     * Identificador único de la planta nuclear.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name; // Nombre de la planta
-    private String location; // Ubicación
+    /**
+     * Nombre de la planta nuclear.
+     */
+    private String name;
 
+    /**
+     * Ubicación de la planta nuclear.
+     */
+    private String location;
+
+    /**
+     * Fecha y hora en que se creó la planta nuclear.
+     */
     @CreatedDate
     private LocalDateTime createdDate;
 
+    /**
+     * Fecha y hora en que se modificó por última vez la planta nuclear.
+     */
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 
+    /**
+     * Usuario que creó la planta nuclear.
+     */
     @CreatedBy
     private String createdBy;
 
+    /**
+     * Usuario que modificó por última vez la planta nuclear.
+     */
     @LastModifiedBy
     private String lastModifiedBy;
 
