071241b28c692aebb5feee06773c253a81deae68
ismahl95 <ismael.h.lamela@gmail.com>
2025-10-05 20:11:14 +0200

    fix(mapper): resolve ReactorMapper implementation error
    - Add unmappedTargetPolicy to ignore unmapped fields
    - Update Maven compiler configuration for MapStruct
    - Add proper annotations for bean mapping
    - Improve mapper interface with explicit mappings

--- backend/src/main/java/com/ihl95/nuclear/reactor/application/mapper/ReactorMapper.java
+++ backend/src/main/java/com/ihl95/nuclear/reactor/application/mapper/ReactorMapper.java
@@ -1,16 +1,34 @@
 package com.ihl95.nuclear.reactor.application.mapper;
+
 import java.util.List;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
 import com.ihl95.nuclear.reactor.application.dto.ReactorDTO;
 import com.ihl95.nuclear.reactor.domain.Reactor;
-@Mapper(componentModel = "spring")
-public interface ReactorMapper {
-    ReactorMapper INSTANCE = Mappers.getMapper(ReactorMapper.class);
 
-    List<ReactorDTO> reactorListToReactorDTOList(List<Reactor> reactors);
-    List<Reactor> reactorDTOListToReactorList(List<ReactorDTO> reactorDTOs);
+@Mapper(
+    componentModel = "spring",
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface ReactorMapper {
+
+    @Mapping(target = "nuclearPlant", ignore = true)
+    Reactor toReactor(ReactorDTO reactorDTO);
+
+    ReactorDTO toReactorDTO(Reactor reactor);
+
+    List<ReactorDTO> toReactorDTOList(List<Reactor> reactors);
+    List<Reactor> toReactorList(List<ReactorDTO> reactorDTOs);
+
+    @Mapping(target = "nuclearPlant", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateReactorFromDto(ReactorDTO dto, @MappingTarget Reactor entity);
 }
 
